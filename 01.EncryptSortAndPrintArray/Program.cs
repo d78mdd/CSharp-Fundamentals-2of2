3836c58961bb15c9be023d9347ce9590b488071d
dkostadinov <d78mdd@gmail.com>
2024-04-21 16:24:10 +0300

    add 60/100 solution for 01. Encrypt, Sort and Print Array

--- 01.EncryptSortAndPrintArray/Program.cs
+++ 01.EncryptSortAndPrintArray/Program.cs
@@ -7,6 +7,67 @@ namespace _01.EncryptSortAndPrintArray
     {
         static void Main(string[] args)
         {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+
+
+            // collect the strings
+            string[] sequenceOfStrings = new string[numberOfStrings];
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                sequenceOfStrings[i] = Console.ReadLine();
+            }
+
+
+            // encrypt the strings
+            string vowelsLower = "aeiouyw";
+            string vowelsUpper = "AEIOUYW";
+            string consonantsLower = "bcdfghjklmnpqrstvwxyz";
+            string consonantsUpper = "BCDFGHJKLMNPQRSTVWXYZ";
+            int[] encryptedStrings = new int[numberOfStrings];
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < sequenceOfStrings[i].Length; j++)
+                {
+                    char letter = sequenceOfStrings[i][j];
+
+                    if (vowelsLower.Contains(letter) || vowelsUpper.Contains(letter))
+                    {
+                        sum += letter * sequenceOfStrings[i].Length;
+                    }
+                    else if (consonantsLower.Contains(letter) || consonantsUpper.Contains(letter))
+                    {
+                        sum += letter / sequenceOfStrings[i].Length;
+                    }
+
+                }
+
+                encryptedStrings[i] = sum;
+            }
+
+
+            // sort the encryptions
+            int[] sortedEncryptedStrings = (int[])encryptedStrings.Clone();
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                for (int j = 0; j < numberOfStrings - 1; j++)
+                {
+                    int temp = sortedEncryptedStrings[j];
+                    if (sortedEncryptedStrings[j] > sortedEncryptedStrings[j + 1])
+                    {
+                        sortedEncryptedStrings[j] = sortedEncryptedStrings[j + 1];
+                        sortedEncryptedStrings[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            // print the sorted encryptions
+            foreach (int encryptedString in sortedEncryptedStrings)
+            {
+                Console.WriteLine(encryptedString);
+            }
 
         }
     }
