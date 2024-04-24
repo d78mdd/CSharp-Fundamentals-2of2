b2211dac58866565e3629ddcc6e6038356eb9582
dkostadinov <d78mdd@gmail.com>
2024-04-24 07:48:51 +0300

    add partial solution

--- 05.LongestIncreasingSubsequence/Program.cs
+++ 05.LongestIncreasingSubsequence/Program.cs
@@ -7,12 +7,72 @@ namespace _05.LongestIncreasingSubsequence
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-
-            int[] allNumbers = input
+            int[] allNumbers = Console.ReadLine()
                 .Split(' ')
                 .Select(int.Parse)
                 .ToArray();
+
+            int n = allNumbers.Length;
+
+            int allSubsequencesCount = 0;
+
+            string[] allSubSequences;
+
+
+            //get all subsequences - but only consecutive
+            {
+                // count all subsequences
+
+                // get a starting number
+                for (int i = 0; i < n; i++)
+                {
+                    // get ending number
+                    for (int j = i; j < n; j++)
+                    {
+                        // get all numbers from starting to ending number - construct 1 subsequence
+                        allSubsequencesCount++;
+                        //Console.Write(allSubsequencesCount + "  ");
+                        for (int k = i; k <= j; k++)
+                        {
+                            //Console.Write(allNumbers[k] + " ");
+                        }
+                        //Console.WriteLine();
+
+                    }
+                    //Console.WriteLine();
+                }
+
+
+                // get them
+                allSubSequences = new string[allSubsequencesCount];
+                int subSequenceIndex = 0;
+
+                // get a starting number
+                for (int i = 0; i < n; i++)
+                {
+                    // get ending number
+                    for (int j = i; j < n; j++)
+                    {
+                        // get all numbers from starting to ending number - construct 1 subsequence
+                        for (int k = i; k <= j; k++)
+                        {
+                            allSubSequences[subSequenceIndex] += allNumbers[k] + " ";
+                        }
+                        Console.WriteLine(allSubSequences[subSequenceIndex]);
+                        subSequenceIndex++;
+
+                    }
+                }
+            }
+
+
+
+
+
+            //collect only the increasing ones
+
+
+            //get the longest one
 
         }
     }
